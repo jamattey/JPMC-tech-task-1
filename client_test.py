c7f588d22cbe1d86b8e798d7e8d217a7535bc4d9
James Amattey <thejamesamattey@gmail.com>
2022-04-13 19:58:16 +0000

    Update client_test.py

--- client_test.py
+++ client_test.py
@@ -2,18 +2,23 @@ from typing_extensions import Self
 import unittest
 from client import getDataPoint, getRatio
 
+
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
     quotes = [
-      {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
-      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+      {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453',
+          'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453',
+          'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
-      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
-      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453',
+          'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453',
+          'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
 
@@ -32,8 +37,8 @@ class ClientTest(unittest.TestCase):
 
     """ ------------ Add the assertion below ------------ """
 
-     for quote in quotes:
-    self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'] + quote['top_ask']['price'], quote['top_bid']['price'] + quote['top_ask']['price'])/2))
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'] + quote['top_ask']['price'], quote['top_bid']['price'] + quote['top_ask']['price'])/2))
 
 
     """ ------------ Add unit tests ------------ """
@@ -46,13 +51,13 @@ class ClientTest(unittest.TestCase):
      def test_getRatio_priceAZero(self):
      price_a = 0
      price_b = 121.68
-    +self.assertEqual (getRatio (price_a, price_b))
+    self.assertEqual (getRatio (price_a, price_b))
 
 
      def test_getRatio_greaterThan1(self):
-     price_a = 346.48
-     price_b = 166.39
-     self.assertGreater (getRatio (price_a, price_b), 1)
+      price_a = 346.48
+      price_b = 166.39
+      self.assertGreater (getRatio (price_a, price_b), 1)
     
      def test_getRatio_LessThan1(self):
      price_a = 166.39
